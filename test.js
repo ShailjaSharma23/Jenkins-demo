ac860e36e500a5f7f987260f3f6e0fc4627efaa0
Shailja Sharma <shailjasharma2307@gmail.com>
2026-08-12 10:04:04 +0530

    test cases

--- test.js
+++ test.js
@@ -5,3 +5,13 @@ else{
     console.log("TC-08: Branch Validation Failed");
     passed = false;
 }
+
+if(passed){
+    console.log("TC-10 : Registration Successful : Pass")
+    console.log("/nBuild success");
+    process.exit(0);
+}
+else{
+    console.log("TC-10: Registration successful");
+    console.log
+}
